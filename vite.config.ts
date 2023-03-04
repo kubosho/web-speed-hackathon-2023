7620adf1e1b5268ca05452ca19a7eba27c3ce2a1
Shota Kubota <ta2@o2p.jp>
2023-03-04 17:29:49 +0900

    videoどこにあるんだろうね？

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
-import path from 'node:path';
-
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 
-import { getFileList } from './tools/get_file_list';
-
-const publicDir = path.resolve(__dirname, './public');
-const getPublicFileList = async (targetPath: string) => {
-  const filePaths = await getFileList(targetPath);
-  const publicFiles = filePaths
-    .map((filePath) => path.relative(publicDir, filePath))
-    .map((filePath) => path.join('/', filePath));
-
-  return publicFiles;
-};
-
 export default defineConfig(async () => {
-  const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
-
   return {
     build: {
       assetsInlineLimit: 4096,
@@ -31,7 +15,6 @@ export default defineConfig(async () => {
       ViteEjsPlugin({
         module: '/src/client/index.tsx',
         title: '買えるオーガニック',
-        videos,
       }),
     ],
   };
