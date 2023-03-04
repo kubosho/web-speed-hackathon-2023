1f359dc062f01446abc7808b786ca6f676885010
Shota Kubota <ta2@o2p.jp>
2023-03-05 01:01:44 +0900

    widthの制限はJSで頑張らなくてもいいよね

--- src/client/components/foundation/WidthRestriction/WidthRestriction.tsx
+++ src/client/components/foundation/WidthRestriction/WidthRestriction.tsx
@@ -1,4 +1,4 @@
-import { type FC, type ReactNode, useCallback, useEffect, useRef, useState } from 'react';
+import { type FC, type ReactNode, useRef } from 'react';
 
 import * as styles from './WidthRestriction.styles';
 
@@ -8,28 +8,10 @@ type Props = {
 
 export const WidthRestriction: FC<Props> = ({ children }) => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const [clientWidth, setClientWidth] = useState<number>(0);
-
-  const isReady = clientWidth !== 0;
-
-  const updateClientWidth = useCallback(() => {
-    const width = containerRef.current?.getBoundingClientRect().width ?? 0;
-    // 横幅を最大 1024px にする
-    setClientWidth(Math.min(width, 1024));
-  }, []);
-
-  useEffect(() => {
-    updateClientWidth();
-  }, [updateClientWidth]);
-
-  useEffect(() => {
-    window.addEventListener('resize', updateClientWidth);
-    return () => window.removeEventListener('resize', updateClientWidth);
-  }, [updateClientWidth]);
 
   return (
     <div ref={containerRef} className={styles.container()}>
-      <div className={styles.inner({ width: clientWidth })}>{isReady ? children : null}</div>
+      <div className={styles.inner()}>{children}</div>
     </div>
   );
 };
