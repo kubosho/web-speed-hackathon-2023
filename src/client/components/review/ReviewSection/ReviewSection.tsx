9498dbe8eb640eaddbeaf5b894b7fa73c711e651
Shota Kubota <ta2@o2p.jp>
2023-03-05 11:46:44 +0900

    zodいらなくね？

--- src/client/components/review/ReviewSection/ReviewSection.tsx
+++ src/client/components/review/ReviewSection/ReviewSection.tsx
@@ -1,15 +1,12 @@
 import type { FormikErrors } from 'formik';
 import { useFormik } from 'formik';
 import { type FC, lazy } from 'react';
-import * as z from 'zod';
 
 import type { ReviewFragmentResponse } from '../../../graphql/fragments';
 
 import * as styles from './ReviewSection.styles';
 
 const LESS_THAN_64_LENGTH_REGEX = /^([\s\S\n]{0,8}){0,8}$/u;
-// NOTE: 改行含めて 64 文字以内であるかどうか確認する
-const commentSchema = z.string().regex(LESS_THAN_64_LENGTH_REGEX);
 
 type Props = {
   reviews: ReviewFragmentResponse[] | undefined;
@@ -36,7 +33,7 @@ export const ReviewSection: FC<Props> = ({ hasSignedIn, onSubmitReview, reviews 
     },
     validate(values) {
       const errors: FormikErrors<ReviewForm> = {};
-      if (values.comment != '' && !commentSchema.safeParse(values.comment).success) {
+      if (values.comment != '' && !LESS_THAN_64_LENGTH_REGEX.test(values.comment)) {
         errors['comment'] = '64 文字以内でコメントしてください';
       }
       return errors;
